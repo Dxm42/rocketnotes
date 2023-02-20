a95b5987541d3a290b2c2e1c211383e4723f099a
lucas <lsantana1234@hotmail.com>
2023-02-20 08:59:34 -0300

    :sparkles: feat: Add styled components, created button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
-import "./styles.css"
+import { Container } from './styles';
+
+import { Button } from '../../components/Button';
+
 export  function Details(){
 
   return(
-    <>
+    <Container>
         <h1>Hello world!</h1> 
         <span>lucas santos</span> 
-    </>
+        <Button title="Login" loading/>      
+    </ Container>
   )
 }
