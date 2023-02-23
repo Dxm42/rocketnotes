50d9953ad4749e131993bd5812dd3e879bc9ad26
lucas <lsantana1234@hotmail.com>
2023-02-23 01:37:55 -0300

    :sparkles: feat: criado component input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,10 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from  './styles'
-import { Header }  from '../../components/Header'
+import { Header }  from '../../components/Header';
+import { Input }  from '../../components/Input';
+
+
+import { ButtonText }  from '../../components/ButtonText';
 export function Home(){
     return(
         <Container>
@@ -9,18 +14,25 @@ export function Home(){
 
             <Header />
             <Menu>
-
+                <li><ButtonText title="Todos" isActive /></li>
+                <li><ButtonText title="React" /></li>
+                <li><ButtonText title="Node" /></li>
+                
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo titulo" icon={FiSearch} />
             </Search>
 
             <Content>
 
             </Content>
 
-            <NewNote></NewNote>
+            <NewNote>
+                <FiPlus />
+                    Criar nota
+                
+            </NewNote>
         </Container>
     );
 }
