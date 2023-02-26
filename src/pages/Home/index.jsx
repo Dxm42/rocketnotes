b555b25497c5c29150397dadb581b0b4ea761e0b
lucas <lsantana1234@hotmail.com>
2023-02-26 06:36:52 -0300

    :sparkles: feat: Created component Note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from  './styles'
 import { Header }  from '../../components/Header';
 import { Input }  from '../../components/Input';
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
 
 
 import { ButtonText }  from '../../components/ButtonText';
@@ -23,8 +25,17 @@ export function Home(){
             <Search>
                 <Input placeholder="Pesquisar pelo titulo" icon={FiSearch} />
             </Search>
-
             <Content>
+                <Section title="Minhas Notas">
+                    <Note data={{
+                        title: 'React', 
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'rocketseat'}
+                        ]
+                    }}
+                        />
+                </Section>
 
             </Content>
 
